5406e7c6f1a6e8e4a2af059c821f8dbde1c0432b
Hardelele <Hardelele@yahoo.com>
2020-06-07 18:51:44 +0300

    Create hello world application

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+
+    private static int anInt = 10;
+
+    public static void main(String[] args) {
+        method();
+    }
+
+    private static void method() {
+        System.out.println("method" + anInt);
+    }
 }
